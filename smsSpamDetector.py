a2f92573c8032a0e05a5fe7c4f9e37dd125eb0e2
Riandy <riandy@windowslive.com>
2017-04-02 15:46:17 +0800

    added tf.idf and svm classifier to predict

--- smsSpamDetector.py
+++ smsSpamDetector.py
@@ -4,9 +4,37 @@ import string
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
+from sklearn.svm import SVC
+from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.corpus import stopwords
+from scipy import sparse
+
+# NOTE TO ARNOLD: Need to download nltk package for stopwords
+# pip install nltk
+# open python
+# import nltk
+# nltk.download("stopwords")
+# source : http://stackoverflow.com/questions/26693736/nltk-and-stopwords-fail-lookuperror
+
+# this method removes stop words, puntuations.
+# it returns the "cleaned" string
+def process_text(text):
+    text = text.translate(None, string.punctuation)
+    text = [word for word in text.split() if word.lower() not in stopwords.words('english')]
+    return " ".join(text)
+
+def train_and_score(features,labels):    
+    # split the train and test data
+    features_train, features_test,labels_train, labels_test = train_test_split(features, labels, train_size=0.7, random_state=0)
+
+    # training and testing part --> can change to other classifiers
+    clf = SVC(kernel='sigmoid', gamma=1.0)
+    clf.fit(features_train,labels_train)
+    return clf.score(features_test,labels_test)
+
 
 if __name__ == "__main__":
-    sms = pd.read_csv("spam.csv", encoding='latin-1')
+    sms = pd.read_csv("spam.csv")
     sms = sms.drop(['Unnamed: 2','Unnamed: 3','Unnamed: 4'],axis=1)
     sms = sms.rename(columns = {'v1':'label','v2':'message'})
     
@@ -28,8 +56,27 @@ if __name__ == "__main__":
     sms['message'] = sms['message'].str.replace(r'\d{4,}',' suspectnumber ')
     sms['message'] = sms['message'].str.replace(r'[^\w\s\d]','')
 
-    #print sms.head(20)
+    # print sms.head(20)
 
     vectorizer = CountVectorizer(ngram_range=(1,3), decode_error='ignore', stop_words="english")
     
-    
+    # tf.idf vectorizer
+    messages = sms['message'].copy()
+    messages = messages.apply(process_text)
+    vectorizer = TfidfVectorizer("english")
+    features_tfidf = vectorizer.fit_transform(messages)
+
+    # construct other features
+    features_others = sms.loc[:,['length','count_caps','ratio_caps','count_digits','ratio_digits','count_excl']]
+
+    # combine the two features
+    features = sparse.hstack((features_tfidf,features_others)).A
+
+    print 'training with tfidf features'
+    print 'accuracy = ' + str(train_and_score(features_tfidf,sms['label']))
+
+    print 'training with other features'
+    print 'accuracy = '  + str(train_and_score(features_others,sms['label']))
+
+    print 'training with combined features'
+    print 'accuracy = ' + str(train_and_score(features,sms['label']))
