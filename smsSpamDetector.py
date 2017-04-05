afdeaeaf5047d3ca2cb86f86f86cda160fd6aa7d
Arnold Koroa <arnold.koroa@gmail.com>
2017-04-05 21:26:20 +0800

    fix feature extraction and generalize classifier

--- smsSpamDetector.py
+++ smsSpamDetector.py
@@ -1,82 +1,112 @@
 import numpy as np
 import pandas as pd
-import string
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix
-from sklearn.svm import SVC
-from sklearn.feature_extraction.text import TfidfVectorizer
-from nltk.corpus import stopwords
 from scipy import sparse
+import string
 
-# NOTE TO ARNOLD: Need to download nltk package for stopwords
-# pip install nltk
-# open python
-# import nltk
-# nltk.download("stopwords")
-# source : http://stackoverflow.com/questions/26693736/nltk-and-stopwords-fail-lookuperror
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import precision_recall_fscore_support
 
-# this method removes stop words, puntuations.
-# it returns the "cleaned" string
-def process_text(text):
-    text = text.translate(None, string.punctuation)
-    text = [word for word in text.split() if word.lower() not in stopwords.words('english')]
-    return " ".join(text)
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import BaggingClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.metrics import accuracy_score
 
-def train_and_score(features,labels):    
-    # split the train and test data
-    features_train, features_test,labels_train, labels_test = train_test_split(features, labels, train_size=0.7, random_state=0)
+#Regex for web urls from https://github.com/rcompton/ryancompton.net/blob/master/assets/praw_drugs/urlmarker.py
+WEB_URL_REGEX = r"""(?i)\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:'".,<>?])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\b/?(?!@)))"""
 
-    # training and testing part --> can change to other classifiers
-    clf = SVC(kernel='sigmoid', gamma=1.0)
-    clf.fit(features_train,labels_train)
-    return clf.score(features_test,labels_test)
-
-
-if __name__ == "__main__":
-    sms = pd.read_csv("spam.csv")
-    sms = sms.drop(['Unnamed: 2','Unnamed: 3','Unnamed: 4'],axis=1)
-    sms = sms.rename(columns = {'v1':'label','v2':'message'})
-    
+def derive_statistical_features(sms):
     sms['length'] = sms['message'].apply(len)
     sms['count_caps'] = sms['message'].apply(lambda msg: len(filter(lambda c: c in string.uppercase, msg)))
     sms['ratio_caps'] = sms['message'].apply(lambda msg: len(filter(lambda c: c in string.uppercase, msg)) / float(len(msg)))
     sms['count_digits'] = sms['message'].apply(lambda msg: len(filter(lambda c: c in string.digits, msg)))
     sms['ratio_digits'] = sms['message'].apply(lambda msg: len(filter(lambda c: c in string.digits, msg)) / float(len(msg)))
     sms['count_excl'] = sms['message'].apply(lambda msg: len(filter(lambda c: c == '!', msg)))
+
+    # normalise count features to [0,1]
+    sms['length'] = sms['length'].apply(lambda count: float(count) / sms['length'].max())
+    sms['count_caps'] = sms['count_caps'].apply(lambda count: float(count) / sms['count_caps'].max())
+    sms['count_digits'] = sms['count_digits'].apply(lambda count: float(count) / sms['count_digits'].max())
+    sms['count_excl'] = sms['count_excl'].apply(lambda count: float(count) / sms['count_excl'].max())
+
+def clean_message(sms):
+    sms['message'] = sms['message'].apply(string.lower)
+    sms['message'] = sms['message'].str.replace(WEB_URL_REGEX,' someurl ')
+    sms['message'] = sms['message'].str.replace(r'[^\w\s]','')
+    sms['message'] = sms['message'].str.replace(r'\d{5,}',' suspectnumber ')
+
+def get_preprocessed_data(datafile):
+    sms = pd.read_csv(datafile, encoding='latin-1')
+    sms = sms.drop(['Unnamed: 2','Unnamed: 3','Unnamed: 4'],axis=1)
+    sms = sms.rename(columns = {'v1':'label','v2':'message'})
     
-    #print sms.head()
-    #print '\nham'
-    #print sms[sms['label'] == 'ham'].describe()
-    #print '\nspam'
-    #print sms[sms['label'] == 'spam'].describe()
+    derive_statistical_features(sms)
+    clean_message(sms)
 
-    sms['message'] = sms['message'].str.lower()
-    sms['message'] = sms['message'].str.replace(r'www.\S+',' someurl ')
-    sms['message'] = sms['message'].str.replace(r'\d{4,}',' suspectnumber ')
-    sms['message'] = sms['message'].str.replace(r'[^\w\s\d]','')
+    return sms
 
-    # print sms.head(20)
+def get_classifiers():
+    classifiers = []
+    classifiers.append(LogisticRegression(solver='liblinear', penalty='l1'))
+    classifiers.append(SVC(kernel='sigmoid', gamma=1.0))
+    classifiers.append(KNeighborsClassifier(n_neighbors=49))
+    classifiers.append(MultinomialNB(alpha=0.2))
+    classifiers.append(DecisionTreeClassifier(min_samples_split=7, random_state=111))
+    classifiers.append(RandomForestClassifier(n_estimators=31, random_state=111))
+    classifiers.append(AdaBoostClassifier(n_estimators=62, random_state=111))
+    classifiers.append(BaggingClassifier(n_estimators=9, random_state=111))
+    classifiers.append(ExtraTreesClassifier(n_estimators=9, random_state=111))
+    return classifiers
 
-    vectorizer = CountVectorizer(ngram_range=(1,3), decode_error='ignore', stop_words="english")
-    
-    # tf.idf vectorizer
-    messages = sms['message'].copy()
-    messages = messages.apply(process_text)
-    vectorizer = TfidfVectorizer("english")
-    features_tfidf = vectorizer.fit_transform(messages)
+def train_and_score(features, labels, classifiers):
+    # split the train and test data
+    features_train, features_test, labels_train, labels_test = train_test_split(features, labels, train_size=0.7, random_state=0)
 
-    # construct other features
-    features_others = sms.loc[:,['length','count_caps','ratio_caps','count_digits','ratio_digits','count_excl']]
+    # training and testing part --> can change to other classifiers
+    scores = []
+    for clf in classifiers:
+        clf.fit(features_train, labels_train)
+        labels_predicted = clf.predict(features_test)
+        scores.append(precision_recall_fscore_support(labels_test, labels_predicted))
+    return scores
 
-    # combine the two features
-    features = sparse.hstack((features_tfidf,features_others)).A
+def str_precision_recall_fscore(scores):
+    s = '      precision recall fscore support\n'
+    s += 'ham : ' + '%0.3f' % scores[0][0] + '     ' + '%0.3f' % scores[1][0] 
+    s += '  ' + '%0.3f' % scores[2][0] + '  ' + str(scores[3][0]) + '\n'
+    s += 'spam: ' + '%0.3f' % scores[0][1] + '     ' + '%0.3f' % scores[1][1] 
+    s += '  ' + '%0.3f' % scores[2][1] + '  ' + str(scores[3][1]) + '\n'
+    return s
+
+def print_precision_recall_fscore(scores, classifiers):
+    for i in range(len(scores)):
+        print classifiers[i].__class__.__name__
+        print str_precision_recall_fscore(scores[i])
+
+if __name__ == "__main__":
+    sms = get_preprocessed_data('spam.csv')
+
+    vectorizer = TfidfVectorizer(ngram_range=(1,1), decode_error='ignore', stop_words='english')
+    features_tfidf = vectorizer.fit_transform(sms['message'])
+    features_others = sms.as_matrix(columns=['length','count_caps','ratio_caps','count_digits','ratio_digits','count_excl'])
+    features = sparse.hstack((features_tfidf, features_others))
+
+    classifiers = get_classifiers()
 
     print 'training with tfidf features'
-    print 'accuracy = ' + str(train_and_score(features_tfidf,sms['label']))
+    scores = train_and_score(features_tfidf, sms['label'], classifiers)
+    print_precision_recall_fscore(scores, classifiers)
 
     print 'training with other features'
-    print 'accuracy = '  + str(train_and_score(features_others,sms['label']))
+    scores = train_and_score(features_others, sms['label'], classifiers)
+    print_precision_recall_fscore(scores, classifiers)
 
     print 'training with combined features'
-    print 'accuracy = ' + str(train_and_score(features,sms['label']))
+    scores = train_and_score(features, sms['label'], classifiers)
+    print_precision_recall_fscore(scores, classifiers)
