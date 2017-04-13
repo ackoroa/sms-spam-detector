e73d88b17ab61d1072156858b0f097dfe3e2bf0c
Arnold Koroa <arnold.koroa@gmail.com>
2017-04-13 22:21:46 +0800

    Commit submission code

--- smsSpamDetector.py
+++ smsSpamDetector.py
@@ -5,19 +5,18 @@ import string
 
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
+
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import precision_recall_fscore_support
 
-from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import AdaBoostClassifier
-from sklearn.ensemble import BaggingClassifier
 from sklearn.ensemble import ExtraTreesClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.ensemble import AdaBoostClassifier
 
 #Regex for web urls from https://github.com/rcompton/ryancompton.net/blob/master/assets/praw_drugs/urlmarker.py
 WEB_URL_REGEX = r"""(?i)\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:'".,<>?])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\b/?(?!@)))"""
@@ -55,14 +54,16 @@ def get_preprocessed_data(datafile):
 def get_classifiers():
     classifiers = []
     classifiers.append(LogisticRegression(solver='liblinear', penalty='l1'))
+    classifiers.append(LogisticRegression(solver='liblinear', penalty='l2'))
+    classifiers.append(SVC(kernel='linear'))
     classifiers.append(SVC(kernel='sigmoid', gamma=1.0))
-    classifiers.append(KNeighborsClassifier(n_neighbors=49))
     classifiers.append(MultinomialNB(alpha=0.2))
-    classifiers.append(DecisionTreeClassifier(min_samples_split=7, random_state=111))
-    classifiers.append(RandomForestClassifier(n_estimators=31, random_state=111))
-    classifiers.append(AdaBoostClassifier(n_estimators=62, random_state=111))
-    classifiers.append(BaggingClassifier(n_estimators=9, random_state=111))
-    classifiers.append(ExtraTreesClassifier(n_estimators=9, random_state=111))
+    classifiers.append(KNeighborsClassifier(n_neighbors=49))
+    classifiers.append(DecisionTreeClassifier(min_samples_split=7))
+    classifiers.append(RandomForestClassifier(n_estimators=31))
+    classifiers.append(ExtraTreesClassifier(n_estimators=9))
+    classifiers.append(AdaBoostClassifier(n_estimators=62))
+    
     return classifiers
 
 def train_and_score(features, labels, classifiers):
@@ -70,25 +71,29 @@ def train_and_score(features, labels, classifiers):
     features_train, features_test, labels_train, labels_test = train_test_split(features, labels, train_size=0.7, random_state=0)
 
     # training and testing part --> can change to other classifiers
+    scores_train = []
     scores = []
     for clf in classifiers:
         clf.fit(features_train, labels_train)
+        labels_predicted_train = clf.predict(features_train)
         labels_predicted = clf.predict(features_test)
+        scores_train.append(precision_recall_fscore_support(labels_train, labels_predicted_train))
         scores.append(precision_recall_fscore_support(labels_test, labels_predicted))
-    return scores
+    return scores, scores_train
 
-def str_precision_recall_fscore(scores):
-    s = '      precision recall fscore support\n'
-    s += 'ham : ' + '%0.3f' % scores[0][0] + '     ' + '%0.3f' % scores[1][0] 
-    s += '  ' + '%0.3f' % scores[2][0] + '  ' + str(scores[3][0]) + '\n'
-    s += 'spam: ' + '%0.3f' % scores[0][1] + '     ' + '%0.3f' % scores[1][1] 
-    s += '  ' + '%0.3f' % scores[2][1] + '  ' + str(scores[3][1]) + '\n'
+def str_precision_recall_fscore(scores, scores_train, classifier):
+    s = classifier + ',ham,' + '%0.3f' % scores_train[0][0] + ',' + '%0.3f' % scores_train[1][0] + ',' + '%0.3f' % scores_train[2][0]
+    s += ',' + '%0.3f' % scores[0][0] + ',' + '%0.3f' % scores[1][0] + ',' + '%0.3f' % scores[2][0] + '\n'
+    s += ',spam,' + '%0.3f' % scores_train[0][1] + ',' + '%0.3f' % scores_train[1][1] + ',' + '%0.3f' % scores_train[2][1]
+    s += ',' + '%0.3f' % scores[0][1] + ',' + '%0.3f' % scores[1][1] + ',' + '%0.3f' % scores[2][1]
     return s
 
-def print_precision_recall_fscore(scores, classifiers):
+def print_precision_recall_fscore(scores, scores_train, classifiers):
+    print 'Classifier,Class,Training,,,Test,,'
+    print ',,precision,recall,fscore,precision,recall,fscore'
     for i in range(len(scores)):
-        print classifiers[i].__class__.__name__
-        print str_precision_recall_fscore(scores[i])
+        print str_precision_recall_fscore(scores[i], scores_train[i], classifiers[i].__class__.__name__)
+    print ''
 
 if __name__ == "__main__":
     sms = get_preprocessed_data('spam.csv')
@@ -101,14 +106,35 @@ if __name__ == "__main__":
 
     classifiers = get_classifiers()
 
-#    print 'training with tfidf features'
-#    scores = train_and_score(features_tfidf, sms['label'], classifiers)
-#    print_precision_recall_fscore(scores, classifiers)
-
-#    print 'training with other features'
-#    scores = train_and_score(features_others, sms['label'], classifiers)
-#    print_precision_recall_fscore(scores, classifiers)
-
     print 'training with combined features'
-    scores = train_and_score(features, sms['label'], classifiers)
-    print_precision_recall_fscore(scores, classifiers)
+    scores, scores_train = train_and_score(features, sms['label'], classifiers)
+    print_precision_recall_fscore(scores, scores_train, classifiers)
+
+    #print vectorizer.vocabulary_
+    coef = classifiers[0].coef_[0]#.A[0]
+    ham_coef_idx = np.argpartition(coef, 10)[10:]
+    spam_coef_idx = np.argpartition(coef, -10)[-10:]
+
+    print 'ham'
+    for i in range(10):
+        for name, idx in vectorizer.vocabulary_.iteritems():
+            if idx == ham_coef_idx[i]:
+                print i, name, coef[idx]
+                break
+    print ''
+    print 'spam'
+    for i in range(10):
+        found = False
+        for name, idx in vectorizer.vocabulary_.iteritems():
+            if idx == spam_coef_idx[i]:
+                found = True
+                print i, name, coef[idx]
+                break
+        if not found:
+            print i, '(not found)', spam_coef_idx[i], coef[spam_coef_idx[i]]
+    print ''
+
+    print 'tf-idf feature length:', features_tfidf.shape[1]
+    print 'rest of features:', 'length,count_caps,ratio_caps,count_digits,ratio_digits,count_excl'
+    for i in range(features_tfidf.shape[1], len(coef)):
+        print i, coef[i]
